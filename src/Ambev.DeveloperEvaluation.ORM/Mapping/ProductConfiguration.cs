d513c210a9e0ce7cefaa6fc928849c81760fdf7b
Willimar Rocha <willimar@gmail.com>
2025-02-20 15:30:48 -0300

    Migration para produtos

---
+++ src/Ambev.DeveloperEvaluation.ORM/Mapping/ProductConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Text.RegularExpressions;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping;
+
+public class ProductConfiguration : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.ToTable("Products");
+
+        builder.HasKey(u => u.Id);
+        builder.Property(u => u.Id).HasColumnType("uuid").HasDefaultValueSql("gen_random_uuid()");
+
+        builder.Property(u => u.Title).IsRequired().HasMaxLength(100);
+        builder.Property(u => u.Description).IsRequired().HasMaxLength(250);
+        builder.Property(u => u.Category).IsRequired().HasMaxLength(100);
+        builder.Property(u => u.Image).HasMaxLength(1000);
+
+        builder.Property(u => u.Price).IsRequired();
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.ORM/Migrations/20250220182814_ProductStructure.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ambev.DeveloperEvaluation.ORM.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductStructure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false, defaultValueSql: "gen_random_uuid()"),
+                    Title = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Price = table.Column<decimal>(type: "numeric", nullable: false),
+                    Description = table.Column<string>(type: "character varying(250)", maxLength: 250, nullable: false),
+                    Category = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Image = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
