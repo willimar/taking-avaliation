282627eb18f065d239b14a30e0afaed6dbaa804b
Willimar Rocha <willimar@gmail.com>
2025-02-21 13:19:49 -0300

    Entidade de venda

--- src/Ambev.DeveloperEvaluation.Domain/Entities/Sale.cs
+++ src/Ambev.DeveloperEvaluation.Domain/Entities/Sale.cs
@@ -3,20 +3,54 @@ using Ambev.DeveloperEvaluation.Domain.Enums;
 
 namespace Ambev.DeveloperEvaluation.Domain.Entities
 {
+    /// <summary>
+    /// Represents a Sale in the system.
+    /// </summary>
     public class Sale : BaseEntity
     {
+        /// <summary>
+        /// Gets or sets the Sale Number.
+        /// </summary>
         public int Number { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Sale Date.
+        /// </summary>
         public DateTime Date { get; set; }
 
+        /// <summary>
+        /// Gets or sets the Customer Name.
+        /// </summary>
         public string CustomerName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the Customer CPF/CNPJ.
+        /// </summary>
         public string CpfCnpjCustomer { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the Company Name.
+        /// </summary>
         public string CompanyName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Gets or sets the User Name.
+        /// </summary>
         public string UserName { get; set; } = string.Empty;
 
+        /// <summary>
+        /// Gets or sets the Total Value of the Sale.
+        /// </summary>
         public decimal TotalValue { get; set; }
 
+        /// <summary>
+        /// Gets or sets the Sale Status.
+        /// </summary>
         public SaleStatus Status { get; set; }
 
+        /// <summary>
+        /// Gets or sets the Products of the Sale.
+        /// </summary>
         public ICollection<SaleProduct> Products { get; set; } = [];
     }
 }
--- src/Ambev.DeveloperEvaluation.Domain/Entities/SaleProduct.cs
+++ src/Ambev.DeveloperEvaluation.Domain/Entities/SaleProduct.cs
@@ -7,16 +7,44 @@ using System.Threading.Tasks;
 
 namespace Ambev.DeveloperEvaluation.Domain.Entities
 {
+    /// <summary>
+    /// Represents a product in a sale
+    /// </summary>
     public class SaleProduct: BaseEntity
     {
+        /// <summary>
+        /// The Id of the Sale
+        /// </summary>
+        public Guid SaleId { get; set; }
+
+        /// <summary>
+        /// The Name of the Product
+        /// </summary>
         public string ProductName { get; set; } = string.Empty;
 
+        /// <summary>
+        /// The Value of the Product in the sale day
+        /// </summary>
         public decimal UnitValue { get; set; }
+
+        /// <summary>
+        /// The discount of the product in the sale day
+        /// </summary>
         public decimal Discount { get; set; }
+
+        /// <summary>
+        /// The total value of the product in the sale day
+        /// </summary>
         public decimal TotalUnityValue { get; set; }
 
+        /// <summary>
+        /// If the product was canceled in the sale
+        /// </summary>
         public bool Canceled { get; set; }
 
+        /// <summary>
+        /// The quantity of the product in the sale
+        /// </summary>
         public int Count { get; set; }
     }
 }
